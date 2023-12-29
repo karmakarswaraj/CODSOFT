2d58225fb3751e2734be32d166c95f9f4eca8574
Swaraj Karmakar <swarajkarmakar5@gmail.com>
2023-12-30 02:35:31 +0530

    Update

--- Landing page/script.js
+++ Landing page/script.js
@@ -1,34 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Smooth scrolling for anchor links
-    const scrollLinks = document.querySelectorAll('a[href^="#"]');
-    scrollLinks.forEach((link) => {
-      link.addEventListener("click", smoothScroll);
-    });
-  
-    // Responsive navigation menu
-    const menuIcon = document.querySelector(".menu-icon");
-    const menuList = document.querySelector(".menu-list");
-  
-    if (menuIcon) {
-      menuIcon.addEventListener("click", toggleMenu);
-    }
-  
-    function smoothScroll(e) {
-      e.preventDefault();
-  
-      const targetId = this.getAttribute("href");
-      const targetElement = document.querySelector(targetId);
-  
-      if (targetElement) {
-        window.scrollTo({
-          top: targetElement.offsetTop - 60, // Adjust as needed
-          behavior: "smooth",
-        });
-      }
-    }
-  
-    function toggleMenu() {
-      menuList.classList.toggle("show-menu");
-    }
+  // Smooth scrolling for anchor links
+  const scrollLinks = document.querySelectorAll('a[href^="#"]');
+  scrollLinks.forEach((link) => {
+    link.addEventListener("click", smoothScroll);
   });
-  
+
+  // Responsive navigation menu
+  const menuIcon = document.querySelector(".menu-icon");
+  const menuList = document.querySelector(".menu-list");
+
+  if (menuIcon) {
+    menuIcon.addEventListener("click", toggleMenu);
+  }
+
+  function smoothScroll(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 60, // Adjust as needed
+        behavior: "smooth",
+      });
+    }
+  }
+
+  function toggleMenu() {
+    menuList.classList.toggle("show-menu");
+  }
+});
