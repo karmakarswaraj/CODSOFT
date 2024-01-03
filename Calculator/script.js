cde99212e49ca3b30e441f4e24a093d02059834d
Swaraj Karmakar <swarajkarmakar5@gmail.com>
2024-01-03 21:32:51 +0530

    Updated

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,19 +1,20 @@
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button) => {
-    button.addEventListener('click', (e) => {
-        if (e.target.innerHTML == '=') {
-            string = eval(string);
-            document.querySelector('input').value = string;
+const buttons= document.querySelectorAll("input[type='button']");
+const dispalyInput= document.getElementById("displayInput");
+const opeartion= document.getElementById("operation")
+
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if(btn.value == "="){
+            opeartion.innerHTML= dispalyInput.value
+            dispalyInput.value= eval(dispalyInput.value)
+        }else if(btn.value== "C"){
+            opeartion.innerHTML= ""
+            dispalyInput.value= ""
+        }else if(btn.value == "Del"){
+            dispalyInput.value= dispalyInput.value.slice(0, -1)
+        }else{
+            dispalyInput.value += btn.value;
         }
-        else if (e.target.innerHTML == 'C') {
-            string = " ";
-            document.querySelector('input').value = string;
-        }
-        else {
-            console.log(e.target);
-            string = string + e.target.innerHTML;
-            document.querySelector('input').value = string;
-        }
-    });
-});
+    })
+})
